4ff7896f0b7e11dcdd0832bd684a81e5e4fdc975
Ben Skach <ben.skach@gmail.com>
2017-02-22 13:40:25 -0500

    added 3rd picture and back button

--- photo_gallery/photogallery-script.js
+++ photo_gallery/photogallery-script.js
@@ -1,7 +1,9 @@
 var modal = document.getElementById('myModal');
 
-var img1 = document.getElementById('photo1');
-var img2 = document.getElementById('photo2');
+var img1 = document.getElementsByClassName("photo")[0];
+var img2 = document.getElementsByClassName("photo")[1];
+var img3 = document.getElementsByClassName("photo")[2];
+
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
@@ -17,6 +19,12 @@ img2.onclick = function() {
 	captionText.innerHTML = this.alt;
 }
 
+img3.onclick = function() {
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+}
+
 var close = document.getElementsByClassName("close-button")[0];
 close.onclick = function() {
 	modal.style.display = "none";
@@ -24,13 +32,37 @@ close.onclick = function() {
 
 var next = document.getElementsByClassName("next-button")[0];
 next.onclick = function() {
-	if (modalImg.src == document.getElementById('photo1').src) {
-		modalImg.src = document.getElementById('photo2').src;
-		captionText.innerHTML = document.getElementById('photo2').alt;
+	if (modalImg.src == document.getElementsByClassName("photo")[0].src) {
+		modalImg.src = document.getElementsByClassName("photo")[1].src;
+		captionText.innerHTML = document.getElementsByClassName("photo")[1].alt;
 	}
-	else if(modalImg.src == document.getElementById('photo2').src) {
-		modalImg.src = document.getElementById('photo1').src;
-		captionText.innerHTML = document.getElementById('photo1').alt;
+	else if(modalImg.src == document.getElementsByClassName("photo")[1].src) {
+		modalImg.src = document.getElementsByClassName("photo")[2].src;
+		captionText.innerHTML = document.getElementsByClassName("photo")[2].alt;
+	}
+	else if(modalImg.src == document.getElementsByClassName("photo")[2].src) {
+		modalImg.src = document.getElementsByClassName("photo")[0].src;
+		captionText.innerHTML = document.getElementsByClassName("photo")[0].alt;
+	}
+	else {
+		console.log("YOU MESSED UP");
+		modal.style.display = "none";
+	}
+}
+
+var back = document.getElementsByClassName("back-button")[0];
+back.onclick = function() {
+	if (modalImg.src == document.getElementsByClassName("photo")[0].src) {
+		modalImg.src = document.getElementsByClassName("photo")[2].src;
+		captionText.innerHTML = document.getElementsByClassName("photo")[2].alt;
+	}
+	else if(modalImg.src == document.getElementsByClassName("photo")[1].src) {
+		modalImg.src = document.getElementsByClassName("photo")[0].src;
+		captionText.innerHTML = document.getElementsByClassName("photo")[0].alt;
+	}
+	else if(modalImg.src == document.getElementsByClassName("photo")[2].src) {
+		modalImg.src = document.getElementsByClassName("photo")[1].src;
+		captionText.innerHTML = document.getElementsByClassName("photo")[1].alt;
 	}
 	else {
 		console.log("YOU MESSED UP");
