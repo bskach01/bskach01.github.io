cea13afc3f2db9b9333f874938c7e173f3666a2c
Ben Skach <benskach@Bens-MBP.home>
2017-09-18 11:23:21 -0400

    added outdoor leadership to portfolio

--- README.txt
+++ README.txt
@@ -1,7 +1,1 @@
-Assignment1
-Ben Skach
-February 8, 2017
-
-In this assignment, I created a portfolio website. I included a homepage with a photo, as well as a menu wtih links to my bio and resume. Each of these pages also includes the same menu to increase accesibility. There are 3 html files, one for eache webpage, and a styling css file that applies to all 3 pages. These are all included in this github repository. The url http://www.cs.tufts.ed/~bskach01/ redirects to the github portfolio.
-
-I did not collaborate with anyone on this project, but I used websites such as w3schools and stack overflow extensively to learn tags, improve styling, understand the code, and troubleshoot. I worked on this project for approximately 6 hours.
+This site, originally created as an assignment for my web design class, now contains information about me and my goals. It has my resume, a portfolio, and a bio. It also contains a gallery of my photography and a pdf of an independenty study project I conducted.
--- portfolio-script.js
+++ portfolio-script.js
@@ -6,6 +6,11 @@ var img3 = document.getElementsByClassName("social-post")[2];
 var img4 = document.getElementsByClassName("social-post")[3];
 var img5 = document.getElementsByClassName("social-post")[4];
 var img6 = document.getElementsByClassName("social-post")[5];
+var img7 = document.getElementsByClassName("social-post")[6];
+var img8 = document.getElementsByClassName("social-post")[7];
+var img9 = document.getElementsByClassName("social-post")[8];
+var img10 = document.getElementsByClassName("social-post-vertical")[0];
+var img11 = document.getElementsByClassName("social-post-vertical")[1];
 
 var modalImg = document.getElementById("img01");
 
@@ -35,6 +40,31 @@ img5.onclick = function() {
 }
 
 img6.onclick = function() {
+	modal.style.display = "block";
+	modalImg.src = this.src;
+}
+
+img7.onclick = function() {
+	modal.style.display = "block";
+	modalImg.src = this.src;
+}
+
+img8.onclick = function() {
+	modal.style.display = "block";
+	modalImg.src = this.src;
+}
+
+img9.onclick = function() {
+	modal.style.display = "block";
+	modalImg.src = this.src;
+}
+
+img10.onclick = function() {
+	modal.style.display = "block";
+	modalImg.src = this.src;
+}
+
+img11.onclick = function() {
 	modal.style.display = "block";
 	modalImg.src = this.src;
 }
